a9f4c022160534ec3702549842a8743ead2d190d
alu0100836059 <alu0100836059@ull.edu.es>
2016-03-24 19:03:41 +0000

    primera prueba con mocha chai

---
+++ test/spec.js
@@ -0,0 +1,7 @@
+var expect = chai.expect;
+
+describe('prueba', function() {
+  it('Primera prueba. Las cadenas son iguales', function() {
+    expect('hello world').to.equal('hello world');
+  });
+});
