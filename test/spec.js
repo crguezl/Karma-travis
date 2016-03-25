5a75188e8a170b6e5db713cbf54cb23ce12dd5cc
alu0100836059 <alu0100836059@ull.edu.es>
2016-03-25 13:50:24 +0000

    Borrada prueba inicial y comprobadas las conversiones de Celsius

--- test/spec.js
+++ test/spec.js
@@ -1,15 +1,21 @@
 var expect = chai.expect;
 
-describe('prueba', function() {
-  it('Primera prueba. Las cadenas son iguales', function() {
-    expect('hello world').to.equal('hello world');
-  });
-});
-
 describe('La medida tiene un tipo y un valor', function() {
   it('El tipo y el valor se asignan correctamente', function() {
     var m = new Medida(32,'f');
     expect(m.value).to.equal(32);
     expect(m.type).to.equal('f');
+  });
+});
+
+describe('Conversiones Celsius', function() {
+  it('Celsius convierte bien a Fahrenheit', function() {
+    var c = new Celsius(32,'c','f');
+    expect(c.toFarenheit()).to.equal(89.60);
+    });
+  it('Celsius convierte bien a Kelvin', function() {
+    var ck = new Celsius(32,'c','k');
+    expect(ck.toKelvin()).to.equal(305.15);
+
   });
 });
