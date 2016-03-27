0adb23999c3095cf0c5cf94de9e25a0d8c1820d4
alu0100622492 <alu0100622492@ull.edu.es>
2016-03-27 23:16:09 +0100

    Arreglado package.json y empezando gulpfile, 1 error

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat');
+var del     = require('del');
+
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
+
+
+gulp.task('minify', function () {
+  gulp.src('./assets/js/temperature.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./index.html')
+    .pipe(minifyHTM())
+    .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./*.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
+});
+
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
+});
+
+gulp.task('default', ['minify'], function() {
+});
