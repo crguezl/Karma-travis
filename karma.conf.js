d23589800b14a4e067b7bf411580d2fa91d09e89
alu0100622492 <alu0100622492@ull.edu.es>
2016-04-01 20:19:01 +0100

    funciona mocha y jasmine pero a mitad de proceso se peta

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha','jasmine','chai'],
+    frameworks: ['mocha','jasmine'],
 
     client: {
       mocha: {
